fb5d6ff916b1e520939d3745b2d54dd3997eea6f
Nixon <voxell.technologies@gmail.com>
2023-07-04 22:56:27 +0800

    feat: add BoidConfig

---
+++ Assets/Project/Scripts/GameWorld.AI/Boid/BoidConfig.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BoidConfig : ScriptableObject
+{
+    public float MinSpeed = 2.0f;
+    public float MaxSpeed = 5.0f;
+    public float PerceptionRadius = 2.5f;
+    public float AvoidanceRadius = 1.0f;
+    public float MaxSteerForce = 3.0f;
+
+    public float AlignWeight = 1.0f;
+    public float CohesionWeight = 1.0f;
+    public float SeperateWeight = 1.0f;
+
+    public float TargetWeight = 1.0f;
+
+    [Header ("Collisions")]
+    public LayerMask ObstacleMask;
+    public float BoundsRadius = 0.27f;
+    public float AvoidCollisionWeight = 10.0f;
+    public float CollisionAvoidDst = 5.0f;
+}
