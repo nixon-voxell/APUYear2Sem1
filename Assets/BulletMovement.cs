f7a72f11ff886c5a9b8cbb73005439ac3748f1d6
Kudoshi <brendenguan@live.com>
2023-07-19 13:33:32 +0800

    Feat: Add hitfx on hit stuff

--- Assets/BulletMovement.cs
+++ Assets/BulletMovement.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem m_HitFx;
+    [SerializeField] private MeshRenderer m_MeshRenderer;
+    [SerializeField] private CapsuleCollider m_CapsuleCollider;
+    [SerializeField] private GameObject m_Pfx;
+
     private bool m_Activated = false;
     private float m_BulletLifetime = 3f;
     private float m_BulletSpeed;
 
     private Rigidbody m_Rigidbody;
+    
+
 
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
+
     public void StartBullet(float speed)
     {
+        if (!gameObject.activeSelf)
+        {
+            gameObject.SetActive(true);
+        }
+
         this.m_BulletSpeed = speed;
         m_Activated = true;
-        gameObject.SetActive(true);
+        m_CapsuleCollider.enabled = true;
+        m_MeshRenderer.enabled = true;
+        m_Pfx.SetActive(true);
 
         Invoke("ResetBullet", m_BulletLifetime);
     }
@@ -30,20 +45,32 @@ public class BulletMovement : MonoBehaviour
         {
             m_Rigidbody.velocity = transform.forward * m_BulletSpeed;
         }
+        else if (!m_Activated && m_Rigidbody.velocity != Vector3.zero)
+        {
+            m_Rigidbody.velocity = Vector3.zero;
+        }
     }
 
     private void ResetBullet()
     {
+        Debug.Log(m_Activated);
         if (m_Activated)
         {
             m_Activated = false;
             m_BulletSpeed = 0f;
-            gameObject.SetActive(false);
+            m_CapsuleCollider.enabled = false;
+            m_MeshRenderer.enabled = false;
+            m_Pfx.SetActive(false);
+
         }
-        else
-        {
-            Debug.LogWarning("[POOL WARNING] Exceeded maximum bullet pool");
-        }
-        
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ResetBullet();
+
+        m_HitFx.Play();        
+
     }
 }
