ca76634ff9317fa5e5e0621d3c81665374481658
Nixon <voxell.technologies@gmail.com>
2023-07-23 18:18:45 +0800

    feat: level 1 simple design

--- Assets/Project/Scripts/GameWorld.Util/mathx.cs
+++ Assets/Project/Scripts/GameWorld.Util/mathx.cs
@@ -8,11 +8,13 @@ namespace GameWorld.Util
     [BurstCompile]
     public static class mathxx
     {
+        /// <summary>Flatten 3D coordinate into 2D space. (y axis is removed)</summary>
         public static float2 flatten_3d(in float3 vector)
         {
             return float2(vector.x, vector.z);
         }
 
+        /// <summary>Convert 2D coordinate into 3D space. (y axis is added with a zero value)</summary>
         public static float3 unflatten_2d(in float2 vector)
         {
             return float3(vector.x, 0.0f, vector.y);
--- Assets/Project/Scripts/GameWorld/Enemy/EnemySpawner.cs
+++ Assets/Project/Scripts/GameWorld/Enemy/EnemySpawner.cs
@@ -1,20 +1,44 @@
 using UnityEngine;
+using Unity.Mathematics;
+
+using Random = Unity.Mathematics.Random;
 
 namespace GameWorld
 {
-    using AI;
+    using Util;
 
-    [RequireComponent(typeof(BoidManager))]
+    // [RequireComponent(typeof(BoidManager))]
     public class EnemySpawner : MonoBehaviour
     {
-        [SerializeField] private float m_SpawnRadius;
-        [SerializeField] private BoidManager m_BoidManager;
+        [SerializeField] private float2 m_SpawnSize;
+        private Random m_Random;
+        // private BoidManager m_BoidManager;
+
+        public float3 GetRandomPosition()
+        {
+            float2 halfSpawnSize = this.m_SpawnSize * 0.5f;
+            float2 randPos = this.m_Random.NextFloat2(-halfSpawnSize, halfSpawnSize);
+
+            float2 spawnerPos = mathxx.flatten_3d(this.transform.position);
+            randPos += spawnerPos;
+
+            return mathxx.unflatten_2d(randPos);
+        }
+
+        private void Start()
+        {
+            this.m_Random = Random.CreateFromIndex((uint)this.GetInstanceID());
+            // this.m_BoidManager = this.GetComponent<BoidManager>();
+        }
 
 #if UNITY_EDITOR
         private void OnDrawGizmos()
         {
-            Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.4f);
-            Gizmos.DrawWireSphere(this.transform.position, this.m_SpawnRadius);
+            Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.1f);
+            Gizmos.DrawCube(
+                this.transform.position + Vector3.up * 0.5f,
+                new float3(this.m_SpawnSize.x, 1.0f, this.m_SpawnSize.y)
+            );
         }
 #endif
     }
