a0b6233c3adccd0f4e3e38e458e1e1a8292215ca
Nixon <voxell.technologies@gmail.com>
2023-07-24 19:41:43 +0800

    feat: InGameHUD changes

--- Assets/Project/Scripts/GameWorld.UX/UXManager.cs
+++ Assets/Project/Scripts/GameWorld.UX/UXManager.cs
@@ -6,5 +6,8 @@ namespace GameWorld.UX
 
     public class UXManager : SingletonMono<UXManager>
     {
+        [HideInInspector] public MainMenu MainMenu;
+        // [HideInInspector] public OptionMenu OptionMenu;
+        // [HideInInspector] public InGameHUD InGameHUD;
     }
 }
