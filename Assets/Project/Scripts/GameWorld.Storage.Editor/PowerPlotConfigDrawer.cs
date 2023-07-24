1e9bfc8faa7395e765f16031d97ee72e66e8f04c
Nixon <voxell.technologies@gmail.com>
2023-07-24 18:29:58 +0800

    feat: add PowerPlotConfig custom UI

--- Assets/Project/Scripts/GameWorld.Storage.Editor/PowerPlotConfigDrawer.cs
+++ Assets/Project/Scripts/GameWorld.Storage.Editor/PowerPlotConfigDrawer.cs
@@ -1,4 +1,6 @@
+using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 using UnityEditor;
 
 namespace GameWorld.Storage.Editor
@@ -6,9 +8,65 @@ namespace GameWorld.Storage.Editor
     [CustomPropertyDrawer(typeof(PowerPlotConfig))]
     public class PowerPlotConfigDrawer : PropertyDrawer
     {
+        [SerializeField] private VisualTreeAsset m_Asset;
+
+        private const int PLOT_RANGE = 50;
+        private const int PLOT_GAP = 5;
+        private const float PLOT_HEIGHT = 100.0f;
+
+        private SerializedProperty m_Property;
+
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
-            return base.CreatePropertyGUI(property);
+            this.m_Property = property;
+
+            VisualElement root = new VisualElement();
+
+            PropertyField propertyField = new PropertyField(property);
+            root.Add(propertyField);
+
+            VisualElement plot = new VisualElement();
+            plot.style.height = PLOT_HEIGHT;
+            plot.generateVisualContent += this.DrawPlot;
+            root.Add(plot);
+
+            propertyField.TrackPropertyValue(property, (_) => plot.MarkDirtyRepaint());
+
+            return root;
+        }
+
+        private void DrawPlot(MeshGenerationContext context)
+        {
+            if (this.m_Property == null) return;
+
+            float baseValue = this.m_Property.FindPropertyRelative("BaseValue").floatValue;
+            float multiplier = this.m_Property.FindPropertyRelative("Multiplier").floatValue;
+            float power = this.m_Property.FindPropertyRelative("Power").floatValue;
+
+            float[] plotHeights = new float[PLOT_RANGE];
+
+            for (int p = 0; p < PLOT_RANGE; p++)
+            {
+                plotHeights[p] = PowerPlotConfig.Evaluate(baseValue, multiplier, power, p);
+            }
+
+            Painter2D painter = context.painter2D;
+
+            painter.BeginPath();
+            painter.strokeColor = Color.white;
+            painter.fillColor = Color.white;
+
+            painter.LineTo(new Vector2(0.0f, PLOT_HEIGHT));
+            for (int p = 0; p < PLOT_RANGE; p++)
+            {
+                if (plotHeights[p] > PLOT_HEIGHT)
+                {
+                    continue;
+                }
+                painter.LineTo(new Vector2(p * PLOT_GAP, PLOT_HEIGHT - plotHeights[p]));
+            }
+
+            painter.Stroke();
         }
     }
 }
--- Assets/Project/Scripts/GameWorld.Storage/Configs/WaveConfig.cs
+++ Assets/Project/Scripts/GameWorld.Storage/Configs/WaveConfig.cs
@@ -6,12 +6,19 @@ namespace GameWorld.Storage
     [System.Serializable]
     public struct WaveConfig : IDefault<WaveConfig>
     {
-        public PowerPlotConfig DamagePlot;
+        public PowerPlotConfig MinionDamagePlot;
+        public PowerPlotConfig MinionCountPlot;
+        public PowerPlotConfig BossDamagePlot;
+        public PowerPlotConfig BossCountPlot;
+
         public WaveConfig Default()
         {
             return new WaveConfig
             {
-                
+                MinionDamagePlot = new PowerPlotConfig().Default(),
+                MinionCountPlot = new PowerPlotConfig().Default(),
+                BossDamagePlot = new PowerPlotConfig().Default(),
+                BossCountPlot = new PowerPlotConfig().Default(),
             };
         }
     }
