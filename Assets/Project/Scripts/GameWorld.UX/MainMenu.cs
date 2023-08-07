717563a4fc84902e125e00d6b6c9dd78e797536d
DanyYap <124513296+DanyYap@users.noreply.github.com>
2023-08-07 10:53:03 +0800

    ButtonSoundEffectAdded

--- Assets/Project/Scripts/GameWorld.UX/MainMenu.cs
+++ Assets/Project/Scripts/GameWorld.UX/MainMenu.cs
@@ -10,11 +10,12 @@ namespace GameWorld.UX
         private Button m_OptionBtn;
         private Button m_QuitBtn;
         public AudioClip m_AudioClip;
+        public AudioSource m_MyAudioSource;
 
         private void Start()
         {
             this.InitializeDoc();
-
+            
             this.m_PlayBtn = this.m_Root.Q<Button>("play-btn");
             this.m_OptionBtn = this.m_Root.Q<Button>("option-btn");
             this.m_QuitBtn = this.m_Root.Q<Button>("quit-btn");
@@ -22,17 +23,20 @@ namespace GameWorld.UX
             this.m_PlayBtn.clicked += () =>
             {
                 Debug.Log("Play");
+                m_MyAudioSource.Play();
                 
             };
 
             this.m_OptionBtn.clicked += () =>
             {
                 Debug.Log("Option");
+                m_MyAudioSource.Play();
             };
 
             this.m_QuitBtn.clicked += () =>
             {
                 Debug.Log("Quit");
+                m_MyAudioSource.Play();
                 Application.Quit();
             };
         }
