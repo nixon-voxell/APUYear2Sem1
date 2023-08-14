85efb39c51e0a666ce894df304bee4d6efd7a612
Nixon <voxell.technologies@gmail.com>
2023-08-15 00:17:11 +0800

    feat: add bunch of comps

---
+++ Assets/Project/Scripts/GameWorld/Components/ArmorComp.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace GameWorld
+{
+    public class ArmorComp : MonoBehaviour
+    {
+        /// <summary>Percentage of damage defense.</summary>
+        public float Defense;
+        /// <summary>Health regeneration per second.</summary>
+        public int HealthRegen;
+    }
+}
